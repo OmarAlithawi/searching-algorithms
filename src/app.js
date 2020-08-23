040cd778818bd30147208ef75a15ea3fe59fe84d
Omar Alithawi <oadnan100@gmail.com>
2020-08-23 23:21:30 +0300

    binary search algorithm

--- src/app.js
+++ src/app.js
@@ -52,6 +52,53 @@ const linearSearch = async (arr, value) => {
   }
 };
 
+const binarySearch = async (arr, value) => {
+  const speed = document.querySelector(".textInputSpeed").value;
+  LINEAR_SEARCH_BTN.disabled = true;
+  INPUT_TARGET.disabled = true;
+  parseInt(speed);
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[start].classList.add("start");
+    arr[end].classList.add("end");
+    arr[middle].classList.add("active");
+    await delay(speed);
+    arr[start].classList.remove("start");
+    arr[end].classList.remove("end");
+    arr[middle].classList.remove("active");
+    if (value < arr[middle].textContent) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+
+    if (arr[middle].textContent == value) {
+      arr[middle].classList.add("active");
+      LINEAR_SEARCH_BTN.disabled = false;
+      INPUT_TARGET.disabled = false;
+      return (REASULT.innerHTML = "index of the target = " + middle);
+    }
+    middle = Math.floor((start + end) / 2);
+    if(i === arr.length -1){
+      LINEAR_SEARCH_BTN.disabled = false;
+      INPUT_TARGET.disabled = false;
+      REASULT.innerHTML = "Element not found = " + -1;
+    }
+  }
+};
+
+BINARY_SEARCH_BTN.addEventListener("click", () => {
+  for (let i = 0; i < ALL_VALUES_ARRAY.length; i++) {
+    if (ALL_VALUES_ARRAY[i].classList.contains("active")) {
+      ALL_VALUES_ARRAY[i].classList.remove("active");
+    }
+  }
+  binarySearch(ALL_VALUES_ARRAY, INPUT_TARGET.value);
+});
+
 LINEAR_SEARCH_BTN.addEventListener("click", () => {
   for (let i = 0; i < ALL_VALUES_ARRAY.length; i++) {
     if (ALL_VALUES_ARRAY[i].classList.contains("active")) {
