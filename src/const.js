be20a61d474a1d3d6d72bf9deaf8e0b1de8eff75
Omar Alithawi <oadnan100@gmail.com>
2020-08-24 12:22:52 +0300

    refactoring

---
+++ src/const.js
@@ -0,0 +1,7 @@
+const SEARCHING_FOR_INPUT = document.querySelector(".textInputValue");
+const LINEAR_SEARCH_BTN = document.querySelector(".linear");
+const BINARY_SEARCH_BTN = document.querySelector(".binary");
+const REASULT = document.querySelector(".result");
+const HTML_VALUE_ELEMENTS_ARRAY = document.querySelectorAll(".value");
+const HTML_INDEXE_ELEMENTS_ARRAY = document.querySelectorAll(".index");
+const NUMBERS_ARRAY = [];
